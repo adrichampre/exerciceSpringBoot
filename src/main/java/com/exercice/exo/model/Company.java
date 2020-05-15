d0afe38da717728d39f0abd31a4bd5da50de4cde
Adrien Champredonde <adrien.champredonde@gmail.com>
2020-05-15 17:45:29 +0200

    Create class Company.

---
+++ src/main/java/com/exercice/exo/model/Company.java
@@ -0,0 +1,21 @@
+package com.exercice.exo.model;
+
+public class Company extends Entity{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
