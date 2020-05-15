232ab1915fad38a70dd5211262b6617317c93f0c
Adrien Champredonde <adrien.champredonde@gmail.com>
2020-05-15 17:38:24 +0200

    Create class Entity.

---
+++ src/main/java/com/exercice/exo/model/Entity.java
@@ -0,0 +1,33 @@
+package com.exercice.exo.model;
+
+import java.util.Date;
+
+public abstract class Entity {
+
+    private Long id;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
