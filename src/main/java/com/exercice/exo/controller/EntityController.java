5880e6f2a6cebb92bb92e0f24e8fc7e09f85806c
Adrien Champredonde <adrien.champredonde@gmail.com>
2020-05-16 15:15:15 +0200

    Create request REST.

--- src/main/java/com/exercice/exo/controller/EntityController.java
+++ src/main/java/com/exercice/exo/controller/EntityController.java
@@ -1,22 +1,56 @@
 package com.exercice.exo.controller;
 
+import com.exercice.exo.dao.EntityDAO;
 import com.exercice.exo.model.Company;
+import com.exercice.exo.model.Device;
 import com.exercice.exo.model.Entity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class EntityController {
 
-    //Entity/{id}
+
+    @Autowired
+    private EntityDAO entityDAO;
+
+    /*
+        To get all entities
+        localhost:8080/Entity
+    */
+    @GetMapping(value = "Entity")
+    public List<Entity> listeEntity(){
+        return entityDAO.findAll();
+    }
+
+    /*
+        To get an entity by its id
+        localhost:8080/Entity/{id}
+    */
     @GetMapping(value = "Entity/{id}")
     public Entity afficherEntity(@PathVariable int id){
 
-        Entity entity = new Company((long) 1, new Date(), "Purifly");
+        return entityDAO.findById(id);
+    }
 
-        return entity;
+    /*
+        To save a company
+        localhost:8080/Company}
+    */
+    @PostMapping(value = "/Company")
+    public void ajouterEntity(@RequestBody Company company){
+        entityDAO.save(company);
+    }
+
+    /*
+        To save a device
+        localhost:8080/Device
+    */
+    @PostMapping(value = "/Device")
+    public void ajouterEntity(@RequestBody Device device){
+        entityDAO.save(device);
     }
 }
